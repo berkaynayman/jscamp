91b7f06f94aa2a82f9e9685de66de188b310511e
berkaynayman <berkaynayman4@gmail.com>
2021-10-22 21:34:49 +0300

    DAY-1/ODEV-1 50%

---
+++ DAY-1/ODEV-1/src/app.js
@@ -0,0 +1,23 @@
+// javascript is file-base
+
+// javascript is not type-safe
+var sayi1 = 10
+sayi1 = "Berkay Nayman"
+console.log(sayi1)
+
+let sayi2 = 20
+//let sayi2 = "Berkay"
+//console.log(sayi2)  2 kere tanımlama hatası verir
+
+let sayi3 = 10
+sayi3 = "Nayman"
+let student = {id:1, name:"Berkay"}
+console.log(student) // let kullanmaz isek studend is not defined
+
+//function
+function save(){
+    console.log("saved running")
+}
+save()
+
+// 01:24
